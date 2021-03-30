0fe0303897d53276ee1ce2df093b2568579128d8
huMonster <monster.hu@outlook.com>
2021-03-30 17:47:51 +0800

    【新增】TestController测试2方法

--- ids/src/main/java/com/xtt/ids/web/controller/TestController.java
+++ ids/src/main/java/com/xtt/ids/web/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.xtt.ids.web.controller;
 
 import com.xtt.lib.enums.LogDetailEnum;
+import com.xtt.lib.enums.OperationType;
 import com.xtt.lib.enums.OperationUnit;
 import com.xtt.lib.util.NetworkUtil;
 import com.xtt.lib.util.SnowflakeIdWorker;
@@ -8,6 +9,7 @@ import com.xtt.web.log.LogAnnotation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.xnio.streams.WriterOutputStream;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -27,8 +29,14 @@ public class TestController {
 
     @GetMapping("/test")
 //    @LogAnnotation(detail = LogDetailEnum.USER_ADD, operationUnit = OperationUnit.USER_MANAGEMENT, userId = "5e0076cdef60a761b95a47d0", companyId = "393799836148699136")
-    @LogAnnotation(detail = LogDetailEnum.USER_ADD, operationUnit = OperationUnit.USER_MANAGEMENT, userId = "#user.userId", companyId = "#user.companyId")
-    public void test(HttpServletRequest request){
-        log.info("目标机器：" + NetworkUtil.getIpAddress(request));
+    @LogAnnotation(detail = LogDetailEnum.USER_ADD, operationUnit = OperationUnit.USER_MANAGEMENT, userId = "#user.userId", companyId = "#user.companyId", operationType = OperationType.INSERT)
+    public String test(HttpServletRequest request){
+        return "目标机器：" + NetworkUtil.getIpAddress(request);
+    }
+
+    @GetMapping("/test2")
+    @LogAnnotation(detail = LogDetailEnum.USER_DEL_BY_ID, operationUnit = OperationUnit.USER_MANAGEMENT, userId = "5e0076cdef60a761b95a47d0", companyId = "393799836148699136", operationType = OperationType.DELETE)
+    public String test2(HttpServletRequest request){
+        return "目标机器：" + NetworkUtil.getIpAddress(request);
     }
 }
